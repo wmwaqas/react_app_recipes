c07d6f408f0372b930f152a7795886ed68e01d0b
wmwaqas <wmwaqasmahmood@hotmail.com>
2017-07-25 12:12:06 +0200

    Add tests for App and Title

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Title from './components/Title'
 
 class App extends Component {
   render() {
@@ -8,7 +9,9 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <div>
+          <Title content="All Recipes" />
+          </div>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
